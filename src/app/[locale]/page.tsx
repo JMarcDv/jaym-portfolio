6fd38c471dd0bd856450800c1ae0d69021eb1548
Jean DE VERA <jmdevera19@gmail.com>
2025-01-26 01:11:01 +0100

    Added initial setup for i18 : - Changed file structure : added src/app folder into root - Setup i18 next-intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import First from '@/components/Home/first'
-import Projects from '@/components/Projects/projects';
-import Resume from '@/components/Resume/resume'
-import Footer from '@/components/footer';
+import First from '@/src/components/Home/first'
+import Projects from '@/src/components/Projects/projects';
+import Resume from '@/src/components/Resume/resume'
+import Footer from '@/src/components/footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
