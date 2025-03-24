e4411fb48df213089fe6b3149f8cbc0f0945031f
Jean DE VERA <jmdevera19@gmail.com>
2025-03-24 21:10:05 +0100

    Updated headbar display + Moved Starfield component to higher place

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,21 +6,13 @@ import Footer from '@/src/components/footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
-import Starfield from 'react-starfield';
 
 export default function Home() {
   useEffect(() => {
     AOS.init();
   })
   return (
-    //#9864c8
     <>
-      <Starfield
-        starCount={2500}
-        starColor={[255, 255, 255]}
-        speedFactor={0.1}
-        backgroundColor="black"
-      />
       <First/>
       <Resume/>
       <Projects/>
